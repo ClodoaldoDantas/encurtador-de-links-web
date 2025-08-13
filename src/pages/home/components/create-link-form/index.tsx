ca22782e50b862f98b068515db8d88a87a6c1779
Clodoaldo Dantas <clodoaldodantas8@gmail.com>
2025-08-13 12:31:01 -0300

    feat: update font styles, add form validation, and enhance CreateLinkForm component

--- src/pages/home/components/create-link-form/index.tsx
+++ src/pages/home/components/create-link-form/index.tsx
@@ -1,22 +1,66 @@
+import { zodResolver } from '@hookform/resolvers/zod'
+import { WarningCircleIcon } from '@phosphor-icons/react'
+import { useForm } from 'react-hook-form'
+import * as z from 'zod'
 import { Card } from '../../../../components/ui/card'
 import styles from './styles.module.scss'
 
+const formSchema = z.object({
+	originalUrl: z.url({
+		error: 'URL inválida',
+	}),
+})
+
+type CreateLinkFormData = z.infer<typeof formSchema>
+
 export function CreateLinkForm() {
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<CreateLinkFormData>({
+		resolver: zodResolver(formSchema),
+		defaultValues: {
+			originalUrl: '',
+		},
+	})
+
+	const handleCreateLink = (data: CreateLinkFormData) => {
+		console.log(data)
+	}
+
 	return (
 		<Card>
-			<h2 className={styles.title}>Novo link</h2>
+			<h2 className={styles.title}>Gerar novo link</h2>
 
-			<form className={styles.form}>
-				<div className={styles.inputGroup}>
-					<label htmlFor="original-link">link original</label>
+			<form onSubmit={handleSubmit(handleCreateLink)} className={styles.form}>
+				<div
+					className={styles.formItem}
+					data-state={errors.originalUrl ? 'invalid' : 'default'}
+				>
+					<label className={styles.formLabel} htmlFor="original-link">
+						link original
+					</label>
+
 					<input
 						type="text"
 						id="original-link"
-						placeholder="www.exemplo.com.br"
+						placeholder="https://www.exemplo.com.br"
+						className={styles.formInput}
+						{...register('originalUrl')}
 					/>
+
+					{errors.originalUrl && (
+						<div className={styles.formError}>
+							<WarningCircleIcon size={20} />
+							<span>{errors.originalUrl?.message}</span>
+						</div>
+					)}
 				</div>
 
-				<button type="submit">Salvar link</button>
+				<button className={styles.button} type="submit">
+					Salvar link
+				</button>
 			</form>
 		</Card>
 	)
