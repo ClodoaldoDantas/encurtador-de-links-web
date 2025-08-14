0f0b1641c6f7a7bbe49a444c325563cbd09ac182
Clodoaldo Dantas <clodoaldodantas8@gmail.com>
2025-08-14 08:12:02 -0300

    feat: integrate React Query for data fetching in ShortLinksList, add axios for HTTP requests

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './lib/query-client'
 import { HomePage } from './pages/home/page'
 
 export function App() {
-	return <HomePage />
+	return (
+		<QueryClientProvider client={queryClient}>
+			<HomePage />
+		</QueryClientProvider>
+	)
 }
--- src/pages/home/components/short-links-list/index.tsx
+++ src/pages/home/components/short-links-list/index.tsx
@@ -3,21 +3,27 @@ import {
 	LinkIcon,
 	WarningOctagonIcon,
 } from '@phosphor-icons/react'
+import { useQuery } from '@tanstack/react-query'
 import { Card } from '../../../../components/ui/card'
+import { http } from '../../../../lib/http'
 import type { Link } from '../../../../types/link'
 import { ShortLinkItem } from '../short-link-item'
 import styles from './styles.module.scss'
 
 export function ShortLinksList() {
-	const allLinks: Link[] = [] // This should be replaced with actual data fetching logic
-	const loading = false // This should be replaced with actual loading state
-	const isError = false // This should be replaced with actual error state
+	const { data, isLoading, isError } = useQuery({
+		queryKey: ['short-links'],
+		queryFn: async () => {
+			const response = await http.get<{ links: Link[] }>('/links')
+			return response.data
+		},
+	})
 
 	return (
 		<Card>
 			<h2 className={styles.title}>Meus Links</h2>
 
-			{loading ? (
+			{isLoading ? (
 				<div className={styles.loadingState}>
 					<CircleNotchIcon size={32} />
 					<p>Carregando...</p>
@@ -29,7 +35,7 @@ export function ShortLinksList() {
 				</div>
 			) : (
 				<>
-					{allLinks.length === 0 && (
+					{data?.links.length === 0 && (
 						<div className={styles.emptyState}>
 							<LinkIcon size={32} />
 							<p>Ainda não existem links cadastrados</p>
@@ -37,7 +43,7 @@ export function ShortLinksList() {
 					)}
 
 					<ul className={styles.list}>
-						{allLinks.map((link) => (
+						{data?.links.map((link) => (
 							<ShortLinkItem key={link.id} link={link} />
 						))}
 					</ul>
